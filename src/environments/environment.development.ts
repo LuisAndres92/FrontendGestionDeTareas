0deaf5dc40af40f0267e8970f0c8e129737fc712
Luis Andres Cataño <117051562+LuisAndres92@users.noreply.github.com>
2025-09-21 00:15:40 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    //apiUrlBase: 'https://localhost:44358',
-    //apiUrlBase: 'http://localhost:8080',
-    apiUrlBase: 'https://gestion-de-tareas.azurewebsites.net',
-    production: false
+    apiUrlBase: 'http://localhost:8081',
+    production: true
 };
